930dfd102bd84c0dde73233728074f2b4bb64cec
Kamal <kamalshakiri@cmail.carleton.ca>
2019-10-14 10:49:31 -0400

    JavaDoc for Mushroom

--- src/Mushroom.java
+++ src/Mushroom.java
@@ -1,4 +1,9 @@
-
+/**
+ * Mushroom class is a subclass of Piece.
+ * They are stationary and cannot move while the game is in progress
+ * @author Kamaluddin Shakiri
+ * @version 1.0
+ */
 public class Mushroom extends Piece {
 
     public Mushroom() {
