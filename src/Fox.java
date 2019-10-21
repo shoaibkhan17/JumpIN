f14b0f8b1021067f17e83cdca31752f17e4fda18
KhalilAalab <khalilaalab@cmail.carleton.ca>
2019-10-21 14:00:53 -0400

    Started work on fox class

--- src/Fox.java
+++ src/Fox.java
@@ -6,13 +6,51 @@
 		
 public class Fox extends Animal {
 
+	
+	
+	
     // TODO change 2 different classes head and tail
     public Fox() {
         super(PieceType.FOX);
     }
+    
+	public boolean checkValid(int constNumber, boolean x, int number1, int number2, Square[][] squares) {
+		return true;
+	}
 
 	@Override
 	public boolean move(Location oldLocation, Location newLocation,  Board board) {
-        return true;
+		Square[][] squares = board.getSquares();
+		int x1 = oldLocation.getX();
+		int y1 = oldLocation.getY();
+		int x2 = newLocation.getX();
+		int y2 = newLocation.getY();
+
+		if (oldLocation.equals(newLocation)) {
+			return true;
+		}
+
+		else if (oldLocation.getX() == newLocation.getX()) {
+			if (this.checkValid(x1, true, y1, y2, squares)) {
+				board.movePiece(oldLocation, newLocation, this);
+				return true;
+			}
+
+			return false;
+		}
+
+		else if (oldLocation.getY() == newLocation.getY()) {
+			if (this.checkValid(y1, false, x1, x2, squares)) {
+				board.movePiece(oldLocation, newLocation, this);
+				return true;
+			}
+
+			return false;
+		}
+
+		else {
+			return false;
+		}
 	}
+	
 }
