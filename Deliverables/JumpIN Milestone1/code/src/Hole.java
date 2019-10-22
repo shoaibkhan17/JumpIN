48d9119b9dd1a8d5a8e045b7131e33c070f5a09d
Shoaib Khan <shoaib.arifkhan@gmail.com>
2019-10-21 22:24:23 -0400

    Setting up the deliverables

---
+++ Deliverables/JumpIN Milestone1/code/src/Hole.java
@@ -0,0 +1,85 @@
+/**
+ * Hole is a subclass of the abstract Piece class
+ * It is stationary and cannot be moved.
+ * 
+ * @author Khalil Aalab - 101070879
+ * @author Kamaluddin Shakirki - 101054933
+ * @author Simon Yacoub - 101044159
+ * @author Md Aiman Sharif - 101062765
+ * @author Shoaib Khan - 101033582
+ */
+public class Hole extends Piece {
+
+    /**
+     * Holes cannot be moved.
+     */
+    private static final boolean isMovable = false;
+
+    /**
+     * Holes cannot be selected.
+     */
+    private static final boolean isSelectable = false;
+
+    /**
+     * Inner piece.
+     */
+    private Piece innerPiece;
+
+    /**
+     * Default constructor initializing instance variables
+     */
+    public Hole() {
+        super(PieceType.HOLE, isMovable, isSelectable);
+        this.innerPiece = null;
+    }
+
+    /**
+     * Method which sets the piece
+     * @param piece to be set
+     */
+    public void setPiece(Piece piece) {
+        this.innerPiece = piece;
+    }
+
+    /**
+     * Method which removes the piece from the hole.
+     */
+    public void removePiece() {
+        this.innerPiece = null;
+    }
+
+    /**
+     * Method to get the inner piece.
+     * @return innerPiece piece that gets returned.
+     */
+
+    public Piece getPiece() {
+        return this.innerPiece;
+    }
+
+    /**
+     * Checks whether the hole is occupied.
+     * @return true if the hole is occupied, false if the hole is not occupied.
+     */
+    public boolean isOccupied() {
+        return innerPiece != null;
+    }
+
+    /**
+     * Method to gets the status of the hole.
+     * @return String containing the status of the hole.
+     */
+    public String getStatus() {
+        String text = "Hole is ";
+        text += isOccupied() ? "occupied by a " + innerPiece.getClass().getName() + "." : "not occupied.";
+        return text;
+    }
+
+    /**
+     * String representation and calls to toString methods
+     * @return the inner piece string or the hole string. 
+     */
+    public String toString() {
+        return isOccupied() ? this.innerPiece.toString() : super.toString();
+    }
+}
