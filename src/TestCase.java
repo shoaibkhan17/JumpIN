8cb5940f64126caa1b98c72b46fd3d0204343f7f
Md Aiman Sharif <sharif.aiman@gmail.com>
2019-10-26 12:18:27 -0400

    Work on test cases started

---
+++ src/TestCase.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCase {
+
+	@Test
+	void testBoard() {
+		Board board = new Board(1);
+		
+		
+	}
+}
