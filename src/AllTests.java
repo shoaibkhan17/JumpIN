2f8f873d54f8507a94027817428b3b2af4816473
Md Aiman Sharif <sharif.aiman@gmail.com>
2019-11-02 13:24:30 -0400

    Code Refactored

--- src/AllTests.java
+++ src/AllTests.java
@@ -3,7 +3,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({testLogic.class})
+@SuiteClasses({TestLogic.class})
 public class AllTests {
 
 }
