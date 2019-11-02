831434813aed07bbb6234af1cae2c1ce9de04d57
Shoaib Khan <shoaib.arifkhan@gmail.com>
2019-11-02 15:09:27 -0400

    Gui Restructuring and styling
    restructuring the GUI view and adding some styling to the buttons.

--- src/View.java
+++ src/View.java
@@ -1,46 +1,96 @@
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.*;
 import java.awt.Color;
 import java.awt.GridLayout;
 import javax.swing.JLabel;
 
-public class View extends Board{
+public class View extends Board {
+
+	private static JFrame frame;
+	private Square[][] squares;
+	
+	/**
+	 * Styling variables
+	 */
+	private final static Color MAIN_SQUARE_COLOR = new Color(2, 171, 80);
+	private final static Color CORNOR_SQUARE_COLOR = new Color(37, 177, 73);
+	private final static Border LINE = new LineBorder(Color.white);
+	private final static Border MARGIN = new EmptyBorder(5, 15, 5, 15);
+	private final static Border COMPOUND = new CompoundBorder(LINE, MARGIN);
 	
 	public View() {
 		super(1);
-		JFrame jf = new JFrame("JumpIN");
-		JMenuBar mb = new JMenuBar();
+		this.init();
+		this.run();
+	}
+	
+	private void init() {
+		squares = super.getSquares();
+		this.initFrame();
+		this.initMenu();
+		this.initView();
+	}
+	
+	private void initFrame() {
+		frame = new JFrame("JumpIN");
 		GridLayout grid = new GridLayout(5, 5);
-		
-		jf.setLayout(grid);
-
-		Square squares[][] = super.getSquares();
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
+		frame.setLayout(grid);
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	}
+	
+	private void initMenu() {
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(Color.gray);
+		JMenu menu = new JMenu("Options");
+		JMenuItem item1 = new JMenuItem("Reset");
+		item1.setBackground(Color.gray);
+		JMenuItem item2 = new JMenuItem("Exit");
+		item2.setBackground(Color.gray);
+		item2.addActionListener((event) -> System.exit(0));
+		menu.add(item1);
+		menu.add(item2);
+		menuBar.add(menu);
+		frame.setJMenuBar(menuBar);
+    }
+	
+	private void initView() {
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; y < 5; y++) {
 				String text = "";
-				if (squares[i][j].getPiece() != null) {
-					text = squares[i][j].getPiece().toString();
+				if (squares[x][y].getPiece() != null) {
+					text = squares[x][y].getPiece().toString();
 				} else {
 					text = "EMPTY";
 				}
-
-				JButton button = new JButton(text);
-				jf.add(button);
+				frame.add(View.createButton(text, x % 2 == 0 && y % 2 == 0));
 			}
 		}
-		jf.setSize(800, 800);
-		jf.setVisible(true);
-		
 	}
+	
+	private void run() {
+		frame.setVisible(true);
+	}
+	
 	//ImageIcon image = new ImageIcon(getClass().getResource("Fox.png"));
 	//jf.add(image, BorderLayout.NORTH);
 	//jf.add(new JLabel(image), BorderLayout.NORTH);
 	
+	public static JButton createButton(String text, boolean cornorPiece) {
+		JButton button = new JButton(text);
+		button.setFocusPainted(false);
+		button.setBorderPainted(cornorPiece);
+		button.setBackground(cornorPiece ? CORNOR_SQUARE_COLOR : MAIN_SQUARE_COLOR);
+	  	button.setBorder(COMPOUND);
+	  	return button;
+	}
 	
 	public static void main(String[] args) {
 		View obj = new View();
-
 	}
-
 }
